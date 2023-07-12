39d6e4d5684e7129e5fd2a5a56a1bdba6007acfe
Maioli Tommaso <tmaioli@deloitte.it>
2023-07-12 22:46:21 +0200

    initial commit

---
+++ services/people-service/seed.js
@@ -0,0 +1,20 @@
+// services/people-service/seed.js
+
+'use strict'
+
+const people = [
+  'Stephen King',
+  'Miranda July',
+  'Lewis Carroll',
+  'Martha Schumacher',
+  'Mick Garris',
+  'Dede Gardner'
+]
+
+module.exports = async function ({ entities, logger }) {
+  for (const name of people) {
+    const newPerson = await entities.person.save({ input: { name } })
+
+    logger.info({ newPerson }, 'Created person')
+  }
+}
---
+++ services/people-service/types/Person.d.ts
@@ -0,0 +1,11 @@
+/**
+ * Person
+ * A Person
+ */
+declare interface Person {
+    id?: number;
+    createdAt?: string | null;
+    name: string;
+    updatedAt?: string | null;
+}
+export { Person };
